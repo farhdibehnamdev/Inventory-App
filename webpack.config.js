8ad7b79bd017995a0557365f2a77e5371fe0670b
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-07-27 12:57:50 +0430

    🍱 chore:Added webpack config file, tsconfig and package.json

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/app.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "[name][ext]",
+  },
+  resolve: { extensions: [".ts", ".tsx", ".js"] },
+  devtool: "source-map",
+  devServer: {
+    static: { directory: path.resolve(__dirname, "dist") },
+    port: 5002,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s(a|c)ss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: "/node_modules/",
+        use: [
+          {
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env"] },
+          },
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: "Inventory-App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
