6a7c54a628deacd4b9f81832e0f0027eeefa7bad
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-02 13:24:24 +0430

    ✨ feat : Added open and close funtionality to modal.

--- src/productview.ts
+++ src/productview.ts
@@ -2,19 +2,27 @@ const btn = document.querySelector<HTMLButtonElement>(".inventory__btn");
 const tableThead = document.querySelector<HTMLTableElement>(
   ".table-products thead tr"
 );
+const modalPopup = document.querySelector<HTMLElement>("#myModal")!;
+let span = document.querySelector<HTMLElement>(".close");
 btn?.classList.add("btn-product");
 
 export class ProductView {
+  private _span: HTMLElement;
   constructor() {
     this._createTable();
     this._createModal();
     btn?.addEventListener("click", this._openModal.bind(this));
+    this._span = document.querySelector<HTMLElement>(".close")!;
+    this._span.addEventListener("click", this._closeModal.bind(this));
+    document.addEventListener("click", (e: Event) =>
+      this._closeModalWindowClicked(e)
+    );
   }
 
   _createTable(): void {
     if (tableThead) {
       tableThead.innerHTML = `<th>#</th>
-        <th>Name</th>
+        <th>title</th>
         <th>Quantity</th>
         <th>Picture</th>
         <th>Category</th>
@@ -22,6 +30,15 @@ export class ProductView {
     }
   }
   _createModal() {}
-  _openModal() {}
+  _openModal() {
+    modalPopup.style.display = "block";
+  }
+  _closeModal() {
+    modalPopup.style.display = "none";
+  }
+  _closeModalWindowClicked(e: Event) {
+    console.log("biggg", e?.target);
+    if (e.target === modalPopup) modalPopup.style.display = "none";
+  }
   fillTable(obj: Product | Category) {}
 }
