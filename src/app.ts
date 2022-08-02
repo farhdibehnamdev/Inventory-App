09211f2ca41ec865be59932ba49644000ce43d0d
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-02 13:34:07 +0430

    chore : Added App class to initialize and run web app.

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { ProductView } from "./productview";
+import "./assets/scss/style.scss";
+const productMenu = document.querySelector(".menu__products");
+class App {
+  constructor() {
+    this._init();
+  }
+
+  private _init() {
+    if (productMenu) {
+      const product = new ProductView();
+    } else {
+      const category = new Entity<Category>();
+    }
+  }
+}
+
+const app = new App();
