d106e536cdf2e1d0556c1300feb815dde289d4ca
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-04 15:43:24 +0430

    ✨ feat:Added _renderTable and _tableUIBody to add items to table.

--- src/productview.ts
+++ src/productview.ts
@@ -4,41 +4,42 @@ const btn = document.querySelector<HTMLButtonElement>(".inventory__btn");
 const tableThead = document.querySelector<HTMLTableElement>(
   ".table-products thead tr"
 );
+const tableBody = document.querySelector<HTMLTableElement>(".table__tbody");
 const modalPopup = document.querySelector<HTMLElement>("#myModal")!;
 let span = document.querySelector<HTMLElement>(".close");
 const btnSubmit = document.querySelector<HTMLButtonElement>(".btn-submit");
 const inputTitle = document.querySelector<HTMLInputElement>(".form__title");
 const inputQuantity =
   document.querySelector<HTMLInputElement>(".form__quantity");
+
 btn?.classList.add("btn-product");
 
 export class ProductView {
   private _span: HTMLElement;
   private _product: Entity<IProduct>;
   constructor() {
-    this._createTable();
-    this._createModal();
+    this._createHeaderTable();
     this._product = new Entity<IProduct>();
-
     btn?.addEventListener("click", this._openModal.bind(this));
     this._span = document.querySelector<HTMLElement>(".close")!;
     this._span.addEventListener("click", this._closeModal.bind(this));
     document.addEventListener("click", (e: Event) =>
       this._closeModalWindowClicked(e)
     );
+
     btnSubmit?.addEventListener("click", this._addButtonHandler.bind(this));
   }
 
   _addButtonHandler() {
-    this._product.add(
-      new Product(
-        inputTitle!.value,
-        "software",
-        Number.parseInt(inputQuantity!.value)
-      )
+    const newProduct = new Product(
+      inputTitle!.value,
+      "software",
+      Number.parseInt(inputQuantity!.value)
     );
+    this._product.add(newProduct);
+    this._renderTable();
   }
-  _createTable(): void {
+  private _createHeaderTable(): void {
     if (tableThead) {
       tableThead.innerHTML = `<th>#</th>
         <th>Title</th>
@@ -47,16 +48,29 @@ export class ProductView {
         <th></th>`;
     }
   }
-  _createModal() {}
-  _openModal() {
+  private _openModal() {
     modalPopup.style.display = "block";
   }
-  _closeModal() {
+  private _closeModal() {
     modalPopup.style.display = "none";
   }
   _closeModalWindowClicked(e: Event) {
-    console.log("biggg", e?.target);
     if (e.target === modalPopup) modalPopup.style.display = "none";
   }
-  fillTable(obj: IProduct | ICategory) {}
+  _renderTable(): void {
+    const products = this._product.storage;
+    const allProduct = products.map((product: IProduct, index) =>
+      this._tableUIBody.bind(null, <Product>product, index)
+    );
+    tableBody!.innerHTML += allProduct.join("");
+  }
+  private _tableUIBody(item: Product, id: number) {
+    return `<tr class="table__row odd">
+    <td>${id}</td>
+    <td>${item.title}</td>
+    <td>${item.quantity}</td>
+    <td>${item.category}</td>
+    <td>Delete and Edit</td>
+  </tr>`;
+  }
 }
