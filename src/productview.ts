fc9756b458625e315297935271159ffada39c65a
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-04 14:22:08 +0430

    ♻️ refactor : Refactored a method to adding entities.

--- src/productview.ts
+++ src/productview.ts
@@ -1,30 +1,48 @@
+import { Product } from "./product";
+import Entity from "./entity";
 const btn = document.querySelector<HTMLButtonElement>(".inventory__btn");
 const tableThead = document.querySelector<HTMLTableElement>(
   ".table-products thead tr"
 );
 const modalPopup = document.querySelector<HTMLElement>("#myModal")!;
 let span = document.querySelector<HTMLElement>(".close");
+const btnSubmit = document.querySelector<HTMLButtonElement>(".btn-submit");
+const inputTitle = document.querySelector<HTMLInputElement>(".form__title");
+const inputQuantity =
+  document.querySelector<HTMLInputElement>(".form__quantity");
 btn?.classList.add("btn-product");
 
 export class ProductView {
   private _span: HTMLElement;
+  private _product: Entity<IProduct>;
   constructor() {
     this._createTable();
     this._createModal();
+    this._product = new Entity<IProduct>();
+
     btn?.addEventListener("click", this._openModal.bind(this));
     this._span = document.querySelector<HTMLElement>(".close")!;
     this._span.addEventListener("click", this._closeModal.bind(this));
     document.addEventListener("click", (e: Event) =>
       this._closeModalWindowClicked(e)
     );
+    btnSubmit?.addEventListener("click", this._addButtonHandler.bind(this));
   }
 
+  _addButtonHandler() {
+    this._product.add(
+      new Product(
+        inputTitle!.value,
+        "software",
+        Number.parseInt(inputQuantity!.value)
+      )
+    );
+  }
   _createTable(): void {
     if (tableThead) {
       tableThead.innerHTML = `<th>#</th>
-        <th>title</th>
+        <th>Title</th>
         <th>Quantity</th>
-        <th>Picture</th>
         <th>Category</th>
         <th></th>`;
     }
@@ -40,5 +58,5 @@ export class ProductView {
     console.log("biggg", e?.target);
     if (e.target === modalPopup) modalPopup.style.display = "none";
   }
-  fillTable(obj: Product | Category) {}
+  fillTable(obj: IProduct | ICategory) {}
 }
