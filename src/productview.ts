3db563855b5309a1d2f39550ede0a4a9b2890923
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-05 18:55:43 +0430

    Added _selectCategoryHandler to handling dropdown.

--- src/productview.ts
+++ src/productview.ts
@@ -9,6 +9,9 @@ const modalPopup = document.querySelector<HTMLElement>("#myModal")!;
 let span = document.querySelector<HTMLElement>(".close");
 const btnSubmit = document.querySelector<HTMLButtonElement>(".btn-submit");
 const inputTitle = document.querySelector<HTMLInputElement>(".form__title");
+const categoryElement = document.querySelector<HTMLSelectElement>(
+  ".form__select-category"
+);
 const inputQuantity =
   document.querySelector<HTMLInputElement>(".form__quantity");
 
@@ -17,27 +20,42 @@ btn?.classList.add("btn-product");
 export class ProductView {
   private _span: HTMLElement;
   private _product: Entity<IProduct>;
+  private _categoryValue: string = "";
   constructor() {
     this._createHeaderTable();
     this._product = new Entity<IProduct>();
     btn?.addEventListener("click", this._openModal.bind(this));
     this._span = document.querySelector<HTMLElement>(".close")!;
     this._span.addEventListener("click", this._closeModal.bind(this));
+
     document.addEventListener("click", (e: Event) =>
       this._closeModalWindowClicked(e)
     );
 
+    categoryElement?.addEventListener("change", (e: Event) =>
+      this._selectCategoryHandler(e)
+    );
+
     btnSubmit?.addEventListener("click", this._addButtonHandler.bind(this));
   }
-
-  _addButtonHandler() {
+  private _selectCategoryHandler(e: Event) {
+    const categoryElement = document.querySelector<HTMLSelectElement>("option");
+    const element = categoryElement?.value;
+    // this._categoryValue = element.value;
+    console.log(
+      "salam :: ",
+      categoryElement?.options[categoryElement.selectedIndex].value
+    );
+  }
+  private _addButtonHandler() {
     const newProduct = new Product(
-      inputTitle!.value,
-      "software",
-      Number.parseInt(inputQuantity!.value)
+      inputTitle?.value!,
+      this._categoryValue,
+      Number.parseInt(inputQuantity?.value!)
     );
     this._product.add(newProduct);
     this._renderTable();
+    this._closeModal();
   }
   private _createHeaderTable(): void {
     if (tableThead) {
@@ -54,18 +72,20 @@ export class ProductView {
   private _closeModal() {
     modalPopup.style.display = "none";
   }
-  _closeModalWindowClicked(e: Event) {
+  private _closeModalWindowClicked(e: Event) {
     if (e.target === modalPopup) modalPopup.style.display = "none";
   }
-  _renderTable(): void {
+  private _renderTable(): void {
+    tableBody!.innerText = "";
     const products = this._product.storage;
-    const allProduct = products.map((product: IProduct, index) =>
-      this._tableUIBody.bind(null, <Product>product, index)
-    );
+    console.log("storage__product ::", products);
+    const allProduct = products.map((product: IProduct, index) => {
+      return this._tableUIBody(<Product>product, index);
+    });
     tableBody!.innerHTML += allProduct.join("");
   }
   private _tableUIBody(item: Product, id: number) {
-    return `<tr class="table__row odd">
+    return `<tr class="table__row ${++id % 2 != 0 ? "odd" : ""}">
     <td>${id}</td>
     <td>${item.title}</td>
     <td>${item.quantity}</td>
