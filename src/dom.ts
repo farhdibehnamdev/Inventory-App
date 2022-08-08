30abd047ac14a57b8063c7a00e98d4593192ab85
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-08 12:59:10 +0430

    ♻️ reafactor:Separated dom section for handling dom elements.

---
+++ src/dom.ts
@@ -0,0 +1,18 @@
+export const btnAdd = document.querySelector(".inventory__btn");
+export const btn = document.querySelector<HTMLButtonElement>(".inventory__btn");
+export const tableThead = document.querySelector<HTMLTableElement>(
+  ".table-products thead tr"
+);
+export const tableBody =
+  document.querySelector<HTMLTableElement>(".table__tbody");
+export const modalPopup = document.querySelector<HTMLElement>("#myModal")!;
+export let span = document.querySelector<HTMLElement>(".close");
+export const btnSubmit =
+  document.querySelector<HTMLButtonElement>(".btn-submit");
+export const inputTitle =
+  document.querySelector<HTMLInputElement>(".form__title");
+export const categoryElement = document.querySelector<HTMLSelectElement>(
+  ".form__select-category"
+);
+export const inputQuantity =
+  document.querySelector<HTMLInputElement>(".form__quantity");
