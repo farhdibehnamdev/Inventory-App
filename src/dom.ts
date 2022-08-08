fa205e2dd224d2ef9c4677c40db9414ff6ae9376
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-08 18:17:05 +0430

    💡 Added comment to dom elements.

--- src/dom.ts
+++ src/dom.ts
@@ -1,14 +1,31 @@
-export const btnAdd = document.querySelector(".inventory__btn");
 export const btn = document.querySelector<HTMLButtonElement>(".inventory__btn");
 export const tableThead = document.querySelector<HTMLTableElement>(
   ".table-products thead tr"
 );
 export const tableBody =
   document.querySelector<HTMLTableElement>(".table__tbody");
+//==================================================
+
+/// Menu Elements
+export const selectedMenu = <NodeListOf<HTMLAnchorElement>>(
+  document.querySelectorAll(".menu a")
+);
+export const productMenu = document.querySelector(".menu__products");
+export const categoryMenu = document.querySelector(".menu__categories");
+//==================================================
+
+/// Modal Elements
 export const modalPopup = document.querySelector<HTMLElement>("#myModal")!;
+export const modalContentCategory =
+  document.querySelector<HTMLElement>(".modal__categories");
+export const modalContentProduct =
+  document.querySelector<HTMLElement>(".modal__products");
 export let span = document.querySelector<HTMLElement>(".close");
-export const btnSubmit =
-  document.querySelector<HTMLButtonElement>(".btn-submit");
+export const modalHeader =
+  document.querySelector<HTMLHeadElement>(".modal__header h2");
+//==================================================
+
+/// Modal's Form Elements
 export const inputTitle =
   document.querySelector<HTMLInputElement>(".form__title");
 export const categoryElement = document.querySelector<HTMLSelectElement>(
@@ -16,3 +33,5 @@ export const categoryElement = document.querySelector<HTMLSelectElement>(
 );
 export const inputQuantity =
   document.querySelector<HTMLInputElement>(".form__quantity");
+export const btnSubmit =
+  document.querySelector<HTMLButtonElement>(".btn-submit");
