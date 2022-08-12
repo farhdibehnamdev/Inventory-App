b86e53206b44c1032479317fa1355e2ba462d1bb
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-12 16:31:22 +0430

    chore:Added categoryview to productview constructor
    It's because I need to access the getCategoryData method.

--- src/app.ts
+++ src/app.ts
@@ -4,34 +4,42 @@ import { CategoryView } from "./categoryview";
 import { Types } from "./entity";
 import "./assets/scss/style.scss";
 import Entity from "./entity";
+import { ActiveEntity } from "./ActiveEntity";
 
 class App {
   private _productView: ProductView;
   private _categoryView: CategoryView;
+  private _currentEntity: ActiveEntity;
   constructor() {
-    this._productView = new ProductView(new Entity<IProduct>(Types.IProduct));
+    this._currentEntity = new ActiveEntity(Types.IProduct);
     this._categoryView = new CategoryView(
-      new Entity<ICategory>(Types.ICategory)
+      new Entity<ICategory>(Types.ICategory),
+      this._currentEntity
     );
-    console.log("check ::", this._categoryView);
+    this._productView = new ProductView(
+      new Entity<IProduct>(Types.IProduct),
+      this._currentEntity,
+      this._categoryView
+    );
+
     selectedMenu.forEach((el: HTMLAnchorElement) =>
       el.addEventListener("click", (e: Event) => this._selectedMenuHandler(e))
     );
     this._init();
   }
   private _selectedMenuHandler(e: Event): void {
-    console.log("test ::", this);
     if (e instanceof PointerEvent) {
       let btn = e.target as HTMLButtonElement;
       if (btn.classList.contains("menu__products")) {
         btn.classList.add("menu__selected");
         categoryMenu?.classList.remove("menu__selected");
         this._productView.initUI();
+        this._currentEntity.activeMenu(Types.IProduct);
       } else if (btn.classList.contains("menu__categories")) {
         btn.classList.add("menu__selected");
         productMenu?.classList.remove("menu__selected");
-        console.log("what happend in ::", this._categoryView);
         this._categoryView.initCategoryUI();
+        this._currentEntity.activeMenu(Types.ICategory);
       }
     }
   }
